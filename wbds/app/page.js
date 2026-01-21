30c05dbcaefc4a3507d7ef73d5a81ad6030c0894
Armaan-zsh <shaikhmushrraff68@gmail.com>
2026-01-21 20:18:10 +0530

    done

--- wbds/app/page.js
+++ wbds/app/page.js
@@ -781,7 +781,7 @@ export default function Home() {
                         onClick={handleOpenBottle}
                         title="Pick from the Void"
                     >
-                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M4.93 19.07l1.41-1.41M17.66 6.34l1.41-1.41" />
                         </svg>
                     </button>
